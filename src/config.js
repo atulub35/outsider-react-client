170a1cf9e2502d61f136854a0087f5cfe151306a
Atul Ubarhande <atul612@gmail.com>
2025-04-03 17:30:35 +0530

    updated env

--- src/config.js
+++ src/config.js
@@ -1,10 +1,5 @@
 // Debug environment variables
 console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('REACT_APP_API_URL:', process.env.REACT_APP_API_URL);
 
-// Set API URL based on environment
-const isProduction = process.env.NODE_ENV === 'production';
-console.log('Is Production:', isProduction);
-
-export const API_URL = isProduction 
-  ? 'https://outsider-express-f8ef1ffa744f.herokuapp.com' 
-  : 'http://localhost:3000'; 
+export const API_URL = process.env.REACT_APP_API_URL;
